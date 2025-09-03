59dc70c593e20761795b34fc94e604c9b1a41454
RNS-ago <ercolealfageme@gmail.com>
2025-09-03 02:19:40 +0200

    Deleting old logging method and adding time_diff assertion function

--- src/aywen/utils.py
+++ src/aywen/utils.py
@@ -3,51 +3,15 @@ import datetime as dt
 import pandas as pd
 import logging
 import os
+import numpy as np
+import geopandas as gpd
+
 
 # --- Configure logging globally ---
 logging.basicConfig(
     level=logging.INFO,  # Default level (can be changed later)
     format="%(message)s"  # Only show the message (no timestamps/levels unless you want them)
 )
-
-# --- Decorator ---
-def log_step(func, key="fire_id"):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        tic = dt.datetime.now()
-        result = func(*args, **kwargs)
-        time_taken = str(dt.datetime.now() - tic)
-
-        if isinstance(result, pd.DataFrame):
-            logging.info(
-                f"\033[94m[LOG]\033[0m Just ran step "
-                f"\033[92m{func.__name__}\033[0m, "
-                f"there are \033[93m{len(result[key].unique())}\033[0m unique {key} values, "
-                f"and it took \033[91m{time_taken}\033[0m"
-            )
-
-        elif isinstance(result, list) and all(isinstance(df, pd.DataFrame) for df in result):
-            logging.info(
-                f"\033[94m[LOG]\033[0m Just ran step "
-                f"\033[92m{func.__name__}\033[0m, "
-                f"it returned a list of {len(result)} DataFrames "
-                f"and took \033[91m{time_taken}\033[0m"
-            )
-            for i, df in enumerate(result, 1):
-                df_name = getattr(df, "name", f"df_{i}")
-                logging.info(
-                    f"\t\033[93mDataset {df_name}\033[0m: "
-                    f"{df.shape[0]} rows x {df.shape[1]} columns"
-                )
-
-        else:
-            logging.info(
-                f"\033[94m[LOG]\033[0m Just ran step "
-                f"\033[92m{func.__name__}\033[0m, "
-                f"it took \033[91m{time_taken}\033[0m"
-            )
-        return result
-    return wrapper
 
 
 def concatnate_name_from_paths(paths: list[str]) -> str:
@@ -60,4 +24,50 @@ def concatnate_name_from_paths(paths: list[str]) -> str:
     report_date_range = '_'.join(report_date_range)
     
     return f"{report_type}_{report_date_range}"
+    
+    
+def assert_time_diff(df1, df2, key1, key2, type='equal'):
+
+    left = df1[['fire_id', key1]].copy()
+    right = df2[['fire_id', key2]].copy()
+
+    # cast to datetime
+    left[key1] = pd.to_datetime(left[key1])
+    right[key2] = pd.to_datetime(right[key2])
+
+    # group by fire_id and get min
+    right = right.groupby('fire_id').min().reset_index()
+    #  merge
+    merged = pd.merge(left, right, on='fire_id', how='left')
+
+    # error
+    merged['time_diff'] = (merged[key1] - merged[key2]).dt.total_seconds() / 60
+    merged.dropna(subset=['time_diff'], inplace=True)
+
+    if type == 'equal':
+        assert np.all(merged['time_diff'] == 0), "Error: There are records with non-zero time difference"
+    elif type == 'positive':
+        assert np.all(merged['time_diff'] >= 0), "Error: There are records with non-positive time difference"
+    elif type == 'negative':
+        assert np.all(merged['time_diff'] <= 0), "Error: There are records with non-negative time difference"
+        
+def get_region(shapefile, coords):
+    """
+    Get the region of a point from a shapefile
+    
+    Parameters
+    ----------
+    shapefile : str
+        Path to the shapefile
+    coords : tuple
+        (x, y) coordinates of the point
+        
+    Returns
+    -------
+    region : str
+        The region of the point
+    """
+    
+    # read shapefile
+    gdf = gpd.read_file(shapefile)
     
